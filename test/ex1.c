983a2e3ded6136479e4d1bca81c426455db561e3
Ton Chanh Le <letonchanh@gmail.com>
2022-04-18 15:06:11 +0000

    updated example

--- test/ex1.c
+++ test/ex1.c
@@ -3,8 +3,8 @@ extern int __VERIFIER_nondet_int();
 
 void main() {
     int x = __VERIFIER_nondet_int();
-    // int y = x;
-    if (x * x > 100) {
+    int y = x;
+    if (x * y > 100) {
         //x = x*x + 1;
         return;
     }
